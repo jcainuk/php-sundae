32f05a21c53779bc43be6d9fbfb1d5eec4440613
Jonathan Cain <jcainuk@gmail.com>
2024-03-21 19:42:52 +0000

    creates header template and includes it in index

---
+++ templates/header.php
@@ -0,0 +1,13 @@
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Sundae</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+
+<body class="grey lighten-4">
+  <nav class="white z-depth-0">
+    <div class="container">
+      <a href="#" class="brand-logo brand-text">PHP Sundae</a>
+    </div>
+  </nav>
