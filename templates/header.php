d3a41a4e438a471c0bd6ba207247ed97ced7b7da
Jonathan Cain <jcainuk@gmail.com>
2024-03-21 19:48:55 +0000

    adds styles for logo text and the color of the add a sundae button

--- templates/header.php
+++ templates/header.php
@@ -3,6 +3,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP Sundae</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <style>
+    .brand {
+      background: #cbb09c !important;
+    }
+
+    .brand-text {
+      color: #cbb09c !important;
+    }
+  </style>
 </head>
 
 <body class="grey lighten-4">
