6e29bfabeb98be4e1bf0010646826090ab9302f6
Jonathan Cain <jcainuk@gmail.com>
2024-03-22 20:30:52 +0000

    establishes connection to the database using mysqli for this project (I havent learned PDO yet!)

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
+// Include database configuration
+include('config.php');
+
+// connect to database
+$connection = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+
+
+// check connection
+if (!$connection) {
+  echo 'Connection error' . mysqli_connect_error();
+}
 
 ?>
 <!DOCTYPE html>
