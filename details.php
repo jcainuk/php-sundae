76cef6487e2fe1b58551a019d848aa5993c3902d
Jonathan Cain <jcainuk@gmail.com>
2024-03-26 20:27:11 +0000

    get the details about one specific table entry from the database

--- details.php
+++ details.php
@@ -1,5 +1,27 @@
 <?php
 
+include('config/db_connect.php');
+
+// check GET request is param from global $_GET array
+if (isset($_GET['id'])) {
+
+  $id = mysqli_real_escape_string($connection, $_GET['id']);
+
+  // make sql
+  $sql = "SELECT * FROM sundaes WHERE id = $id";
+
+  // get the query result
+  $result = mysqli_query($connection, $sql);
+
+  // fetch one result as associate array
+  $sundae = mysqli_fetch_assoc($result);
+
+  // free up memory
+  mysqli_free_result($result);
+
+  // close connection to database
+  mysqli_close($connection);
+}
 ?>
 
 <!DOCTYPE html>
